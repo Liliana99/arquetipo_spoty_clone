9df6465d94b90e2dde2c29248c83cc285283662b
Pol Gabarró Mora <pgabarro@atsistemas.com>
2021-11-16 17:14:27 +0100

    fix(flutter): add autogenerate copyWith

--- lib/app/login/blocs/login_state_bloc.dart
+++ lib/app/login/blocs/login_state_bloc.dart
@@ -1,9 +1,13 @@
 
 import 'package:arquetipo_flutter_bloc/app/shared/models/password_form_model.dart';
 import 'package:arquetipo_flutter_bloc/app/shared/models/username_form_model.dart';
+import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
+part 'login_state_bloc.g.dart';
 
+
+@CopyWith()
 class LoginBlocState extends Equatable {
   const LoginBlocState({
     this.status = FormzStatus.pure,
@@ -18,22 +22,6 @@ class LoginBlocState extends Equatable {
   final Password password;
   final bool remember;
   final bool pwdVisibility;
-
-  LoginBlocState copyWith({
-    FormzStatus status,
-    Username username,
-    Password password,
-    bool remember,
-    bool pwdVisibility,
-  }) {
-    return LoginBlocState(
-      status: status ?? this.status,
-      username: username ?? this.username,
-      password: password ?? this.password,
-      remember: remember ?? this.remember,
-      pwdVisibility: pwdVisibility ?? this.pwdVisibility,
-    );
-  }
 
   bool isValid() {
     return Formz.validate([this.username, this.password]).isValid;
